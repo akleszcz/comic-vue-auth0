4a40217f80630df7f2ede543fbdd9d0381fea411
Anna Kleszcz <annakleszcz@onet.pl>
2020-07-20 18:10:37 +0200

    Auth0 SDK wrapper and plugin created

---
+++ client/src/auth/README.md
@@ -0,0 +1,25 @@
+## `install.js`
+Contains a wrapper Vue object around the Auth0 SDK and a Vue plugin (`Auth0Plugin`) that exposes this wrapper to the rest of the application.
+
+Source: [Beginner Vue.js Tutorial with User Login](https://auth0.com/blog/beginner-vuejs-tutorial-with-user-login)
+
+The code has been adjusted for TypeScript.
+
+# Vue plugins
+- usually used to add global-level functionality to Vue
+- should expose an `install` method that has two parameters: `Vue` and `options`
+- used by calling the `Vue.use` global method (before `new Vue()`)
+- `Vue.use` automatically prevents you from using the same plugin more than once
+
+`Auth0Plugin` is used to add some Vue instance methods by attaching them to `Vue.prototype`.
+
+Source: [Vue.js. Plugins](https://vuejs.org/v2/guide/plugins.html)
+
+### Notes on the code:
+```typescript
+const DEFAULT_REDIRECT_CALLBACK = () =>
+  window.history.replaceState({}, document.title, window.location.pathname);
+```
+@TODO:
+https://html.spec.whatwg.org/multipage/history.html
+"Consider a game where the user can navigate along a line..."
---
+++ client/src/auth/index.ts
@@ -0,0 +1,125 @@
+import Vue from "vue";
+import createAuth0Client, { PopupLoginOptions, RedirectLoginOptions, GetIdTokenClaimsOptions, GetTokenSilentlyOptions, GetTokenWithPopupOptions, LogoutOptions, RedirectLoginResult } from "@auth0/auth0-spa-js";
+import { OnRedirectCallbackOptions, Auth0PluginData } from '@/typings/auth0';
+
+const DEFAULT_REDIRECT_CALLBACK = () =>
+  window.history.replaceState({}, document.title, window.location.pathname);
+
+let instance: Vue;
+
+export const getInstance = () => instance;
+
+export const useAuth0 = ({
+  onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
+  redirectUri = window.location.origin,
+  domain,
+  clientId,
+  audience,
+}: OnRedirectCallbackOptions) => {
+  if (instance) return instance;
+
+  instance = new Vue({
+    data(): Auth0PluginData {
+      return {
+        loading: true,
+        isAuthenticated: false,
+        user: {},
+        auth0Client: null,
+        popupOpen: false,
+        error: null
+      };
+    },
+    methods: {
+      async loginWithPopup(o?: PopupLoginOptions) {
+        this.popupOpen = true;
+
+        try {
+          await this.auth0Client?.loginWithPopup(o);
+        } catch (e) {
+          console.error(e);
+        } finally {
+          this.popupOpen = false;
+        }
+
+        this.user = await this.auth0Client?.getUser();
+        this.isAuthenticated = true;
+      },
+
+      async handleRedirectCallback() {
+        this.loading = true;
+        try {
+          await this.auth0Client?.handleRedirectCallback();
+          this.user = await this.auth0Client?.getUser();
+          this.isAuthenticated = true;
+        } catch (e) {
+          this.error = e;
+        } finally {
+          this.loading = false;
+        }
+      },
+
+      loginWithRedirect(o?: RedirectLoginOptions) {
+        return this.auth0Client?.loginWithRedirect(o);
+      },
+
+      getIdTokenClaims(o?: GetIdTokenClaimsOptions) {
+        return this.auth0Client?.getIdTokenClaims(o);
+      },
+
+      getTokenSilently(o?: GetTokenSilentlyOptions) {
+        return this.auth0Client?.getTokenSilently(o);
+      },
+
+      getTokenWithPopup(o: GetTokenWithPopupOptions) {
+        return this.auth0Client?.getTokenWithPopup(o);
+      },
+
+      logout(o?: LogoutOptions) {
+        return this.auth0Client?.logout(o);
+      }
+    },
+    
+    async created() {
+      this.auth0Client = await createAuth0Client({
+        domain: domain,
+        client_id: clientId,
+        audience: audience,
+        redirect_uri: redirectUri
+      });
+
+      try {
+        if (
+          window.location.search.includes("code=") &&
+          window.location.search.includes("state=")
+        ) {
+
+          const { appState }: RedirectLoginResult = await this.auth0Client.handleRedirectCallback();
+
+          onRedirectCallback(appState);
+        }
+      } catch (e) {
+        this.error = e;
+      } finally {
+        this.isAuthenticated = await this.auth0Client.isAuthenticated();
+        this.user = await this.auth0Client.getUser();
+        this.loading = false;
+      }
+    }
+  });
+
+  return instance;
+};
+
+// as described at https://vuejs.org/v2/guide/typescript.html#Augmenting-Types-for-Use-with-Plugins
+declare module 'vue/types/vue' {
+  interface Vue {
+    prototype: any,
+    $auth: string
+  }
+}
+
+export const Auth0Plugin = {
+  install(VueClass: Vue, options: OnRedirectCallbackOptions) {
+    VueClass.prototype.$auth = useAuth0(options);
+  }
+};
